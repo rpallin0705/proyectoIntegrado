fade97b8d014cda931851ad4eea5feca1f00c891
rpallin0705 <145771366+rpallin0705@users.noreply.github.com>
2024-11-09 19:34:26 +0100

    Refactor layouts y logica login (simple)

---
+++ app/src/main/java/com/example/logincardview/LoginActivity.kt
@@ -0,0 +1,55 @@
+package com.example.logincardview
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.logincardview.databinding.LoginActivityBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var loginActivityBinding: LoginActivityBinding
+
+    companion object {
+        private const val USERNAME_CREDENTIAL = "usuario"
+        private const val PASSWORD_CREDENTIAL = "usuario"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        loginActivityBinding = LoginActivityBinding.inflate(layoutInflater)
+        setContentView(loginActivityBinding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        start()
+
+    }
+
+    private fun start() {
+
+        loginActivityBinding.loginButton.setOnClickListener {
+            val username = loginActivityBinding.usernameEditTxt.text.toString()  // Obtener el texto del username
+            val password = loginActivityBinding.passwordEditTxt.text.toString()  // Obtener el texto del password
+
+            if (username == USERNAME_CREDENTIAL && password == PASSWORD_CREDENTIAL) {
+                val intent = Intent(this, MainScreen::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "El usuario o la contraseña son incorrectos", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+}
