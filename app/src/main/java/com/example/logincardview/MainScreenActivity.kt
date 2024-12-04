d81f27e5ced86560026dd886b5438b01ed665390
rpallin0705 <145771366+rpallin0705@users.noreply.github.com>
2024-12-04 16:17:37 +0100

    Botones inferiores (visual)

--- app/src/main/java/com/example/logincardview/MainScreenActivity.kt
+++ app/src/main/java/com/example/logincardview/MainScreenActivity.kt
@@ -1,6 +1,7 @@
 package com.example.logincardview
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentTransaction
@@ -27,7 +28,31 @@ class MainScreenActivity : AppCompatActivity() {
         val username = intent.getStringExtra("username")
         val password = intent.getStringExtra("password")
 
-        mainScreenBinding.userInfo.text = "Usuario: ${username} - Contraseña: ${password}"
+        addButtonListeners()
+    }
+
+    private fun addButtonListeners() {
+        val buttons = listOf(
+            mainScreenBinding.btnHome,
+            mainScreenBinding.btnAdd,
+            mainScreenBinding.btnFavorites
+        )
+
+        buttons.forEach { button ->
+            button.setOnClickListener {
+                // Cambiar el estado de selección
+                updateButtonStates(buttons, button)
+            }
+        }
+
+        updateButtonStates(buttons, mainScreenBinding.btnHome)
+    }
+
+    private fun updateButtonStates(buttons: List<ImageButton>, selectedButton: ImageButton) {
+        buttons.forEach { button ->
+            // Establecer el estado seleccionado solo en el botón presionado
+            button.isSelected = (button == selectedButton)
+        }
     }
 
 }
