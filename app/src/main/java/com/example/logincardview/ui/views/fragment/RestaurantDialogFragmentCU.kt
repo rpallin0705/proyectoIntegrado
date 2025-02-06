6bff14b3e7cdf1842be1363214118baf093c5462
rpallin0705 <145771366+rpallin0705@users.noreply.github.com>
2025-02-06 17:03:59 +0100

    refacor nombre variables

--- app/src/main/java/com/example/logincardview/ui/views/fragment/RestaurantDialogFragmentCU.kt
+++ app/src/main/java/com/example/logincardview/ui/views/fragment/RestaurantDialogFragmentCU.kt
@@ -9,13 +9,12 @@ import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import com.example.logincardview.R
 import com.example.logincardview.databinding.FragmentAddRestaurantBinding
-import com.example.logincardview.models.ArgumentsLocal
-import com.example.logincardview.models.Local
+import com.example.logincardview.domain.models.Restaurant
 
 class RestaurantDialogFragmentCU : DialogFragment() {
 
-    // Callback para devolver el local editado
-    var onUpdate: ((Local) -> Unit)? = null
+    // Callback para devolver el restaurant editado
+    var onUpdate: ((Restaurant) -> Unit)? = null
 
     // Variable para guardar la valoración seleccionada
     private var selectedRating = 0
@@ -26,21 +25,21 @@ class RestaurantDialogFragmentCU : DialogFragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val viewDialogAddLocal = inflater.inflate(R.layout.fragment_add_restaurant, container, false)
+    ): View {
+        val viewDialogAddRestaurant = inflater.inflate(R.layout.fragment_add_restaurant, container, false)
 
-        val btnConfirm = viewDialogAddLocal.findViewById<ImageButton>(R.id.positive_button)
-        val btnCancel = viewDialogAddLocal.findViewById<ImageButton>(R.id.negative_button)
+        val btnConfirm = viewDialogAddRestaurant.findViewById<ImageButton>(R.id.positive_button)
+        val btnCancel = viewDialogAddRestaurant.findViewById<ImageButton>(R.id.negative_button)
 
         // Configurar los valores iniciales
-        setValuesIntoDialog(viewDialogAddLocal, arguments)
+        setValuesIntoDialog(viewDialogAddRestaurant, arguments)
 
         // Inicialización de las estrellas
-        val star1 = viewDialogAddLocal.findViewById<ImageButton>(R.id.star1)
-        val star2 = viewDialogAddLocal.findViewById<ImageButton>(R.id.star2)
-        val star3 = viewDialogAddLocal.findViewById<ImageButton>(R.id.star3)
-        val star4 = viewDialogAddLocal.findViewById<ImageButton>(R.id.star4)
-        val star5 = viewDialogAddLocal.findViewById<ImageButton>(R.id.star5)
+        val star1 = viewDialogAddRestaurant.findViewById<ImageButton>(R.id.star1)
+        val star2 = viewDialogAddRestaurant.findViewById<ImageButton>(R.id.star2)
+        val star3 = viewDialogAddRestaurant.findViewById<ImageButton>(R.id.star3)
+        val star4 = viewDialogAddRestaurant.findViewById<ImageButton>(R.id.star4)
+        val star5 = viewDialogAddRestaurant.findViewById<ImageButton>(R.id.star5)
 
         stars = listOf(star1, star2, star3, star4, star5)
 
@@ -52,12 +51,12 @@ class RestaurantDialogFragmentCU : DialogFragment() {
         }
 
         btnConfirm.setOnClickListener {
-            val updatedLocal = recoverDataLayout(viewDialogAddLocal)
+            val updatedRestaurant = recoverDataLayout(viewDialogAddRestaurant)
 
-            if (updatedLocal.nombre.isEmpty() || updatedLocal.direccion.isEmpty() || updatedLocal.contacto.isEmpty() || updatedLocal.descripcion.isEmpty()) {
+            if (updatedRestaurant.name.isEmpty() || updatedRestaurant.address.isEmpty() || updatedRestaurant.phone.isEmpty() || updatedRestaurant.description.isEmpty()) {
                 Toast.makeText(activity, "Algún campo está vacío", Toast.LENGTH_LONG).show()
             } else {
-                onUpdate?.invoke(updatedLocal)  // Llamar a la función de callback
+                onUpdate?.invoke(updatedRestaurant)  // Llamar a la función de callback
                 dismiss()  // Cerrar el diálogo
             }
         }
@@ -66,10 +65,10 @@ class RestaurantDialogFragmentCU : DialogFragment() {
             dismiss()  // Cerrar el diálogo
         }
 
-        return viewDialogAddLocal
+        return viewDialogAddRestaurant
     }
 
-    private fun recoverDataLayout(view: View): Local {
+    private fun recoverDataLayout(view: View): Restaurant {
         val binding = FragmentAddRestaurantBinding.bind(view)
 
         val name = binding.editLocalName.text.toString()
@@ -77,17 +76,16 @@ class RestaurantDialogFragmentCU : DialogFragment() {
         val phone = binding.editLocalPhone.text.toString()
         val description = binding.editLocalDescription.text.toString()
 
-        return Local(name, address, phone, selectedRating, description)
+        return Restaurant(name, address, phone, selectedRating, description)
     }
 
-    // todo CARGAR VALORACION DEL LOCAL A EDITAR EN EL DIALOGO
-    private fun setValuesIntoDialog(viewDialogEditLocal: View, arguments: Bundle?) {
-        val binding = FragmentAddRestaurantBinding.bind(viewDialogEditLocal)
+    private fun setValuesIntoDialog(viewDialogEditRestaurant: View, arguments: Bundle?) {
+        val binding = FragmentAddRestaurantBinding.bind(viewDialogEditRestaurant)
         arguments?.let {
-            binding.editLocalName.setText(it.getString(ArgumentsLocal.ARGUMENT_NAME))
-            binding.editLocalPhone.setText(it.getString(ArgumentsLocal.ARGUMENT_PHONE))
-            binding.editLocalAddress.setText(it.getString(ArgumentsLocal.ARGUMENT_ADDRESS))
-            binding.editLocalDescription.setText(it.getString(ArgumentsLocal.ARGUMENT_DESCRIPTION))
+            binding.editLocalName.setText(it.getString("name"))
+            binding.editLocalPhone.setText(it.getString("phone"))
+            binding.editLocalAddress.setText(it.getString("address"))
+            binding.editLocalDescription.setText(it.getString("description"))
         }
     }
 
