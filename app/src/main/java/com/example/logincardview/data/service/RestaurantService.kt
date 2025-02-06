defbdf24f561dbdd835c16f029732cf1e41f55d1
rpallin0705 <145771366+rpallin0705@users.noreply.github.com>
2025-02-06 16:53:42 +0100

    Error no mostrar restaurantes terminado

--- app/src/main/java/com/example/logincardview/data/service/RestaurantService.kt
+++ app/src/main/java/com/example/logincardview/data/service/RestaurantService.kt
@@ -1,11 +1,12 @@
 package com.example.logincardview.data.service
 
+import com.example.logincardview.data.datasource.RestaurantDataSource
 import com.example.logincardview.data.models.RestaurantDTO
 import kotlinx.coroutines.delay
 
 class RestaurantService {
     suspend fun getRestaurants(): List<RestaurantDTO> {
-        delay(1000)
-        return getRestaurants()
+        val data = RestaurantDataSource().getRestaurants()
+        return data
     }
 }
